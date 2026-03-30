d2fb5512d2aaf233a14ba15c86f563e7a727a79b
sudeguzeel <150516908+sudeguzeel@users.noreply.github.com>
2026-03-30 12:44:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from keras.models import load_model
+import tensorflow as tf
+from tensorflow.keras.models import load_model
 import numpy as np
 from PIL import Image
 import os
