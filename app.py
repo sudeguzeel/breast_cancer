8398ee0199bb9ec96c7abd781aa6fe65dfd4f496
sudeguzeel <150516908+sudeguzeel@users.noreply.github.com>
2026-03-30 12:40:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 import streamlit as st
-import tensorflow as tf
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 import numpy as np
 from PIL import Image
 import os
-
-
 # SAYFA AYARLARI
 
 st.set_page_config(
